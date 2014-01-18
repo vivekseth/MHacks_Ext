b6773aa756ec1a6f06d9dd49e4b3d33d8fb1df1a
Vivek Seth <Vivekseth.m@gmail.com>
2014-01-18 02:07:58 -0500

    removed unneccesary db stuff

--- background.js
+++ background.js
@@ -1,8 +1,4 @@
-var DATABASE = 'local_database';
-var LOG_TABLE = 'time_logs';
-
 console.log("hello world")
-var db = new ydn.db.Storage(DATABASE);
 
 function createLog(website, url, start_time, end_time) {
 	return {
@@ -12,14 +8,12 @@ function createLog(website, url, start_time, end_time) {
 		"end_time" : end_time,
 	}
 }
-
 function saveLog(l) {
 	chrome.storage.local.get("logs", function(data){
 		data.logs.push(l);
 		chrome.storage.local.set(data);
 	})
 }
-
 function getLogs() {
 	chrome.storage.local.get("logs", function(value){
 		console.log(value)
