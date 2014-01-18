b836e6df9eb31ddef61ae0de06bde494bc2a9427
Vivek Seth <Vivekseth.m@gmail.com>
2014-01-18 02:38:00 -0500

    d

--- database.js
+++ database.js
@@ -8,12 +8,14 @@ function createLog(website, url, start_time, end_time) {
 }
 function saveLog(l) {
 	chrome.storage.local.get("logs", function(data){
+		if (!data) data = {};
+		if (!data.logs) data.logs = [];
 		data.logs.push(l);
 		chrome.storage.local.set(data);
 	})
 }
 function getLogs() {
-	chrome.storage.local.get("logs", function(value){
-		console.log(value)
+	chrome.storage.local.get("logs", function(data){
+		console.log(data)
 	})
 }
