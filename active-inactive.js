f31ff020422789b04ac44ca784ff69df70e544cc
Vaibhav Patel <vpatel1894@gmail.com>
2014-01-18 04:21:37 -0500

    active vs inactive

--- active-inactive.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('focus', function() {
-    document.title = 'focused';
-});
-
-window.addEventListener('blur', function() {
-    document.title = 'not focused';
-});
--- active.js
+++ active.js
@@ -1,7 +1,11 @@
+function active(){
+var totalms=0;
 window.addEventListener('focus', function() {
     document.title = 'focused';
+    
 });
 
 window.addEventListener('blur', function() {
     document.title = 'not focused';
 });
+}
