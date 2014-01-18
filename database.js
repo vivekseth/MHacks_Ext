48f94a1262511d8195f7e05e986e5997b072c5c5
Vivek Seth <Vivekseth.m@gmail.com>
2014-01-18 03:36:01 -0500

    added indexedDB code

--- database.js
+++ database.js
@@ -19,3 +19,68 @@ function getLogs() {
 		console.log(data)
 	})
 }
+
+
+logDB.open = function() {
+	var version = 1;
+  	var request = indexedDB.open("logs", version);
+
+  	request.onupgradeneeded = function(e) {
+	    var db = e.target.result;
+	    // A versionchange transaction is started automatically.
+	    e.target.transaction.onerror = html5rocks.indexedDB.onerror;
+	    if(db.objectStoreNames.contains("logs")) {
+	      db.deleteObjectStore("logs");
+	    }
+	    var store = db.createObjectStore("logs",
+	      {keyPath: "timeStamp"});
+	};
+
+	request.onsuccess = function(e) {
+	  logDB.db = e.target.result;
+	  logDB.getAllLogItems();
+	};
+
+	request.onerror = logDB.onerror;
+}
+
+logDB.addTodo = function(website, url, start_time, end_time) {
+  var db = logDB.db;
+  var trans = db.transaction(["logs"], "readwrite");
+  var store = trans.objectStore("logs");
+  var request = store.put(createLog(website, url, start_time, end_time));
+
+  request.onsuccess = function(e) {
+    // Re-render all the logs's
+    logDB.getAllLogItems();
+  };
+
+  request.onerror = function(e) {
+    console.log(e.value);
+  };
+};
+
+logDB.getAllLogItems = function(callback) {
+  var db = logDB.db;
+  var trans = db.transaction(["logs"], "readwrite");
+  var store = trans.objectStore("logs");
+
+  // Get everything in the store;
+  var keyRange = IDBKeyRange.lowerBound(0);
+  var cursorRequest = store.openCursor(keyRange);
+
+  cursorRequest.onsuccess = function(e) {
+    var result = e.target.result;
+    if(!!result == false)
+      return;
+
+    callback(result.value);
+    result.continue();
+  };
+
+  cursorRequest.onerror = logDB.onerror;
+};
+
+logDB.onerror = function(err) {
+	console.logs(err);
+}
