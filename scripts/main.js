9845bf8d47b0052acd57ff952ccc14967477bd11
Vivek Seth <Vivekseth.m@gmail.com>
2014-01-19 07:01:30 -0500

    added notes for pranav

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
+app.addedPopup = false;
+
 function isBlockedSite(url) {
 	for (var i = 0; i < app.blocked_sites.length; i++) {
 		var host1 = getHostName(url);
 		var host2 = getHostName(app.blocked_sites[i]);
-		if (host1 == host2) {
+		if (host1.match(host2) || host2.match(host1)) {
 			return true;
 		}
 	}
@@ -18,6 +20,7 @@ function getHostName(url) {
 app.small_ticker = setInterval(function(){smallTickFunc();}, app.SMALL_TICK_INT);
 var smallTickFunc = function(){
 	chrome.tabs.getSelected(null, function(tab) {
+	  console.log(tab);
 	  tabUrl = tab.url;
 	  if (!app.log.tracking && isBlockedSite(tabUrl)) {
 	  	enteredBlockedSite()
@@ -31,7 +34,7 @@ function enteredBlockedSite() {
 	app.log.tracking = true;
   	app.log.hostname = getHostName(tabUrl);
   	app.log.start_time = new Date();
-  	app.big_ticker = setInterval(function(){bigTickFunc();}, app.BIG_TICK_INT);
+  	app.big_ticker = setInterval(function(){bigTickFunc();}, 1000);
 }
 function leftBlockedSite() {
 	clearInterval(app.big_ticker);
@@ -42,12 +45,18 @@ function leftBlockedSite() {
   	app.log.hostname = null;
   	app.log.end_time = null;
   	app.log.start_time = null;
+  	app.addedPopup = false;
 }
 function bigTickFunc() {
+	console.log(app.BIG_TICK_INT)
 	var minutes = app.BIG_TICK_INT/(1000 * 60);
-	if (minutes < 0) return;
-	chrome.tabs.executeScript(null, {file: "popup.js"});
-	chrome.tabs.executeScript({code: "addPopupWithFile('./pop/index.html', 500, 500)"});
+	//if (minutes < 0) return;
+	//chrome.tabs.executeScript(null, {file: "popup.js"});
+	if (!app.addedPopup) {
+		//chrome.tabs.executeScript({code: "function centerElement(e){e.style.position=\"fixed\";e.style.top=\"50%\";e.style.left=\"50%\";var t=e.clientWidth;var n=e.clientWidth;e.style.marginTop=-n/2+\"px\";e.style.marginLeft=-t/2+\"px\"}function addPopup(e,t,n){var r='<div id=\"__shadow\"><div id=\"__popup\"></div></div><style>#__popup {width: '+t+\"px;height: \"+n+\"px;background-color:#fff}#__shadow {position:absolute;width: 100%;height: 100%;background-color: rgba(0, 0, 0, 0.2);top:0px;}</style>\";var i=document.createElement(\"span\");i.innerHTML=r;document.body.appendChild(i.firstChild);document.body.appendChild(i.firstChild);document.getElementById(\"__popup\").innerHTML=e;centerElement(document.getElementById(\"__popup\"))}addPopup(\"hello world<div>hello world</div>\",500,500)"});
+		chrome.tabs.executeScript(null, {file: "scripts/popup.js"});
+		app.addedPopup = true;
+	}
 }
 
 
